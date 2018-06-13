23f126191014fcbbc0ba9a629d6e29ec74543082
keonsam <keonsamuel95@gmail.com>
2018-06-13 15:23:14 -0400

    appspec file

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,4 @@ set -e
 source /home/ec2-user/.bash_profile
 
 cd /home/ec2-user/Troumaca-web-server
-forever start server.js
+pm2 start http-server -- name app -- dist -p 80
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/Troumaca-web-server
-forever stopall
+pm2 stop all
