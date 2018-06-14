9a6ce630224c4119fba90f311c3d2ac9651763bc
Keon Samuel <keonsamuel95@gmail.com>
2018-06-14 19:44:46 -0400

    run 2

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,5 @@ set -e
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/Troumaca-web
 /usr/bin/screen ~/.nvm/versions/node/v10.4.1/bin/ws --spa index.html -p 4200
-ctrl+a ctrl+d
+ctrl+a
+ctrl+d
