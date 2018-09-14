9f0bc03cd8ff337385ba095ceece1dc71a525967
keonsam <keonsamuel95@gmail.com>
2018-09-14 12:53:37 -0400

    page not found error

--- src/app/page-not-found/page.not.found.module.ts
+++ src/app/page-not-found/page.not.found.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {PageNotFoundComponent} from './page.not.found.component';
 import {PageNotFoundService} from './page.not.found.service';
@@ -10,7 +9,6 @@ import {RouterModule} from '@angular/router';
 @NgModule({
   imports: [
     CommonModule,
-    NgbModule,
     RouterModule,
     FormsModule,
     ReactiveFormsModule
