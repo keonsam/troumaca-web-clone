fee1c32b5a38313c9509a0450a6506ee5145d7be
keonsam <keonsamuel95@gmail.com>
2019-02-19 15:09:33 -0400

    fixs

--- src/authentication/authentication.component.ts
+++ src/authentication/authentication.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {HOME} from '../app/routes';
 
 @Component({
   selector: 'authentication',
@@ -6,5 +7,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./authentication.component.css']
 })
 export class AuthenticationComponent {
-
+  homeLink = `/${HOME}`;
 }
