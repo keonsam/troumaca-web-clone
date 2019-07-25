ecacc12fb93db541ef35aa1dd4d9afbd53401194
keonsam <keonsamuel95@gmail.com>
2019-07-25 09:12:39 -0400

    move to auth servers

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // hostPort: 'http://ec2-18-204-213-58.compute-1.amazonaws.com:3000'
-  hostPort: 'ec2-54-172-191-78.compute-1.amazonaws.com:3000'
+  hostPort: 'http://ec2-54-172-191-78.compute-1.amazonaws.com:3000'
 };
